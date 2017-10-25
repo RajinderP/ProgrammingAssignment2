591e10e0a2e9eb3b4836dacf97d51c8720121c38
RajinderP <33091583+RajinderP@users.noreply.github.com>
2017-10-25 16:43:04 +0100

    RPSolution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##list of functions allowing user to define and retrieve valus for the initial matrix and the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Finds the inverse of the matrix if it hasn't been cached before
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setsolve(m)
+        m
 }
+
+#Example
+mCM <- makeCacheMatrix()
+x <- matrix(1:4,2,2)
+mCM$set(x)
+cacheSolve(mCM)
+cacheSolve(mCM)
